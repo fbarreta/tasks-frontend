32411db541490f38368e229c2d29e86ff3d2bf03
fbarreta <fbarreta@gmail.com>
2023-07-02 10:03:56 -0700

    Readme update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app tittle', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Marvelous v2.0/i);
   expect(linkElement).toBeInTheDocument();
 });
